f6a11c092e89d4b44cfea2afbae69db292afc533
sivaranjani-ps <79299285+sivaranjani-ps@users.noreply.github.com>
2022-02-08 13:52:08 +0530

    initial setup

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,7 +15,7 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
    this._homeservice.checknode().subscribe((data) => {
             console.log(data);
-            alert(' node working');
+            alert(' node working in staging server');
         });
 
   }

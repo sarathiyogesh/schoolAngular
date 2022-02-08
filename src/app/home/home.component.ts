5b66a2d029714c6bbd22cf11a250680dd2020c78
sivaranjani-ps <79299285+sivaranjani-ps@users.noreply.github.com>
2022-02-08 14:38:33 +0530

    Latest changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,15 +7,15 @@ import { HomeService } from './home.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  public nodeWorking:boolean = false;
+  public users:any = [];
   constructor(
   	private _homeservice: HomeService
   ) { }
 
   ngOnInit(): void {
    this._homeservice.checknode().subscribe((data) => {
-            console.log(data);
-            alert(' node working in staging server');
+            this.nodeWorking = true;
         });
 
   }
